6d3ebcb7c214b2ca9d4d99900dba40f01d6c04a4
Samuel Calderon Duque <samuelcalderon@MacBook-Air-de-Samuel-4.local>
2026-02-18 21:05:04 -0500

    feat(products): implement basic product creation

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, contactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import homePageView, aboutPageView, contactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreatedView
 
 urlpatterns = [
     path('', homePageView.as_view(), name='home'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('contact/', contactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/created', ProductCreatedView.as_view(), name='product-created'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ]
