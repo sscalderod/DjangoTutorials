a8443928c9b99f5f4bb8e70b884f0af72083f0ba
Samuel Calderon Duque <samuelcalderon@MacBook-Air-de-Samuel-4.local>
2026-02-25 16:26:22 -0500

    feat(cart): implement basic cart system with session support

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, contactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import homePageView, aboutPageView, contactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', homePageView.as_view(), name='home'),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
