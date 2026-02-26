7e7a9ffe5798ab2419623efc29c71e81f41974ad
Samuel Calderon Duque <samuelcalderon@MacBook-Air-de-Samuel-4.local>
2026-02-26 09:33:33 -0500

    feat/fix-accounts-url-routing

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -23,14 +23,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("", include('pages.urls'))
-    
+    path("", include('pages.urls')),
+    path("accounts/", include("accounts.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    path("", include('pages.urls')),
-    path("accounts/", include("accounts.urls")),
 
 
