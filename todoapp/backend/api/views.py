15d2740e9fd6326d774ad78aaa138dcf60c90d47
Samuel Calderon Duque <samuelcalderon@MacBook-Air-de-Samuel-4.local>
2026-03-05 09:56:04 -0500

    feat: add API system (todoapp)

---
+++ todoapp/backend/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .serializers import ToDoSerializer
+from todo.models import ToDo
+ 
+class ToDoListCreate(generics.ListCreateAPIView):
+# ListCreateAPIView requires two mandatory attributes, serializer_class and
+# queryset.
+# We specify ToDoSerializer which we have earlier implemented
+    serializer_class = ToDoSerializer
+ 
+    def get_queryset(self):
+        user = self.request.user
+        return ToDo.objects.filter(user=user).order_by('-created')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
