7793fa01d1e5c511435d6a4a2dac62c23948c2a3
Samuel Calderon Duque <samuelcalderon@MacBook-Air-de-Samuel-4.local>
2026-03-08 15:33:54 -0500

    feat: add API signup and login authentication

--- todoapp/backend/backend/settings.py
+++ todoapp/backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'todo.apps.TodoConfig',
     'rest_framework',
     'api.apps.ApiConfig',
-    
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+    'rest_framework.authentication.TokenAuthentication',
+    ]
+}
